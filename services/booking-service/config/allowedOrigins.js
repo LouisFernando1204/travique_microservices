22c11c8fc4c1e27076c40fe3dbda2370bc3ab87e
Louis Fernando <ferlouis55@gmail.com>
2025-05-09 16:07:29 +0700

    First init (set up Node.js + Docker for booking-service)

---
+++ services/booking-service/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigin = [
+    // 'https://roomie-dapp.vercel.app',
+    'http://127.0.0.1:5500',
+    'http://localhost:3500',
+    'http://localhost:5173'
+];
+
+module.exports = allowedOrigin;
