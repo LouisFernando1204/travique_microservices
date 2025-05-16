63aaff7c0d4d45865abbc42bc9613f73cf5ad1af
Louis Fernando <ferlouis55@gmail.com>
2025-05-16 16:26:36 +0700

    Testing Booking's Service on Docker (nginx + mongodb work successfully)

--- services/booking-service/config/dbConn.js
+++ services/booking-service/config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI)
+        await mongoose.connect(process.env.NODE_ENV == "development" ? process.env.DATABASE_URI_LOCAL : process.env.DATABASE_URI_PROD);
     } catch (err) {
         console.log(err)
     }
